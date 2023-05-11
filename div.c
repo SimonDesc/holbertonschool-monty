d400e58e6528a3b4ad6fb6319fa21f0e50bd8450
SimonDesc <pro.descombes@gmail.com>
2023-05-11 16:07:37 +0200

    fix: spellcheck on error return

--- div.c
+++ div.c
@@ -15,7 +15,7 @@ void divi(stack_t **stack, unsigned int line_number)
 
 	if (*stack == NULL || (*stack)->next == NULL)
 	{
-		fprintf(stderr, "L%d: can't add, stack too short\n", line_number);
+		fprintf(stderr, "L%d: can't div, stack too short\n", line_number);
 		cleanup();
 		exit(EXIT_FAILURE);
 	}
